2486588b988b973c21f8a25d7d3d0da42aae2569
Emiliano Pineda <emi.rock11@hotmail.com>
2018-08-24 17:17:38 -0500

    Agregar Primera idea de videojuego :pig:

---
+++ Segundo proyecto/HolaMundo3/Assets/Scripts/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+
+    public bool DoRotation = false;
+    public Vector3 move;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        DoRotation = !DoRotation;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (DoRotation) { 
+            transform.Rotate(Vector3.up);
+        }
+    }
+}
---
+++ Segundo proyecto/HolaMundo3/Assets/Scripts/Sphere.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+    // Use this for initialization
+    public float moveX;
+    public Vector3 move;
+
+    Color GetRandomColor() {
+        float r = Random.Range(0.0f, 1.0f);
+        float g = Random.Range(0.0f, 1.0f);
+        float b = Random.Range(0.0f, 1.0f);
+        return new Color(r,g,b);
+    }
+
+    void ChangeColor() {
+        gameObject.GetComponent<Renderer>().material.color = this.GetRandomColor();
+    }
+
+
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            this.ChangeColor();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+            this.gameObject.transform.Translate(move);
+        }
+	}
+
+    void OnMouseDown(){
+        this.ChangeColor();
+    }
+
+
+}
